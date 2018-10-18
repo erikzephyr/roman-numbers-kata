41d0b35bbccef1ab40d720f0708b78155c5abc63
Erik Barreto de Vera <erikbv99421@gmail.com>
2018-10-18 11:09:54 +0100

    saving const info about roman numbers

--- src/index.js
+++ src/index.js
@@ -1,4 +1,15 @@
-function sum(a, b) {
-  return a + b;
-}
-module.exports = sum;
+const ROMAN_NUMBERS = {
+  1000: "M",
+  900: "CM",
+  500: "D",
+  400: "CD",
+  100: "C",
+  90: "XC",
+  50: "L",
+  40: "XL",
+  10: "X",
+  9: "IX",
+  5: "V",
+  4: "IV",
+  1: "I"
+};
